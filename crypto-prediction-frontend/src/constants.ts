e4a58e34b72aed9dcce8149ab08e73a03b36e828
Pradise2 <akinsunmitaye@gmail.com>
2025-08-09 02:30:20 +0100

    contract

--- crypto-prediction-frontend/src/constants.ts
+++ crypto-prediction-frontend/src/constants.ts
@@ -2,15 +2,241 @@
 
 // 1. Paste your contract addresses for each chain
 export const PREDICTION_MARKET_CONTRACT_ADDRESSES: { [chainId: number]: `0x${string}` } = {
-  8453: '0xYourBaseContractAddressHere',      // Base Mainnet
-  56: '0xYourBscContractAddressHere',          // BSC Mainnet
-  10: '0xYourOptimismContractAddressHere',      // Optimism Mainnet
-  42220: '0xYourCeloContractAddressHere',       // Celo Mainnet
-  42161: '0xYourArbitrumContractAddressHere',   // Arbitrum One
+  8453: '0xEcfAC045A72E8D55eB131F4eAFa6b53E5eEC6315',      // Base Mainnet
+  56: '0x696629321C9Aa6A9a446B3447E4887169EB3e0d3',          // BSC Mainnet
+  10: '0x70f4Ea0cc5D0a8f153B5808B23c712FC3C60dF99',      // Optimism Mainnet
+  42220: '0x739c7C73Be569fEd06621BF007bd4505Fd1F3fD8',       // Celo Mainnet
+  42161: '0x739c7C73Be569fEd06621BF007bd4505Fd1F3fD8',   // Arbitrum One
   // Add testnet chain IDs and addresses if needed
 };
 
 // 2. Paste the ABI you copied from Remix
 export const PREDICTION_MARKET_ABI = [
-    // ... paste the entire JSON array here ...
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_priceTarget",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_resolutionTimestamp",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "_oracle",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "enum PredictionMarket.Position",
+				"name": "position",
+				"type": "uint8"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "BetPlaced",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "finalPrice",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "enum PredictionMarket.Position",
+				"name": "winningPosition",
+				"type": "uint8"
+			}
+		],
+		"name": "MarketResolved",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "WinningsClaimed",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "betsDown",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "betsUp",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "claimWinnings",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "oracle",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "enum PredictionMarket.Position",
+				"name": "_position",
+				"type": "uint8"
+			}
+		],
+		"name": "placeBet",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "priceTarget",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "resolutionTimestamp",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "resolveMarket",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "state",
+		"outputs": [
+			{
+				"internalType": "enum PredictionMarket.MarketState",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalBetsDown",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalBetsUp",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
 ] as const;
